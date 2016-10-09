694f6b465ca5b696c8c715695f244d0ce8fdf1f9
Subil <subil1407@gmail.com>
2016-10-09 20:47:38 +0530

    Initial commit

---
+++ processtext.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# This contains the class ProcessText and the subclasses that are created
+# for each textfile type (currently only classes for txt and html).
+
+
+class ProcessText():
+
+    def __init__(self):
+        word_dictionary = {}
+
+    def add_to_dict(self):
+        pass
+
+    def strip_parse_line(self):
+        pass
---
+++ wordcounter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Fairly simple python program (or is it a library? I don't these things)
+# to obtain a JSON of the count of all the words in a file
+
+import processtext
+
+
+class WordCount():
+
+    def __init__(self, filepath=None):
+        self.__filepath = filepath
+
+    def identify_filetype(self):
+        pass
+
+    def get_file(self):
+        pass
+
+    def call_processtext(self):
+        pass
+
+    def get_JSON(self):
+        pass
+
+    def get_result(self):
+        pass
+
+
